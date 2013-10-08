f61cb9a5e1d185b29283c6775ab5d7a43199f0bd
WilsonKoder111 <FaZeWilly@Wilson-M-Koders-Mac-Book-Air.local>
2013-10-08 17:30:38 +0800

    Initial Commit

---
+++ CarApp/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  CarApp
+//
+//  Created by Wilson M Koder on 10/8/13.
+//  Copyright (c) 2013 Wilson-Lui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
